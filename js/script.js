c415412340551e234e785f71631e4da5ff8a4972
Endelion1 <122155353+Endelion1@users.noreply.github.com>
2023-01-07 18:35:04 +0700

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function back() {
+	window.onclick=document.location.href='ISregister.php';
+}
+function last(){
+	window.onclick= alert('Закрыто на доработку');
+}
+function err(){
+	window.onclick= alert('Для этого действия нужно войти в аккаунт или создать');
+}
+function vinlevel(){
+	window.onclick=document.location.href='levelone.php';
+}
